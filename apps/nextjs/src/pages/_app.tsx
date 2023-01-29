1a6f20977e252a1e4bf1079991daac2fe304b6e2
ErickCReis <erickcorreareis@gmail.com>
2023-01-29 00:45:55 -0300

    feat: Adiciona Vercel Analytics

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import { DefaultLayout } from "~/components/default-layout";
 import { api } from "~/utils/api";
@@ -14,11 +15,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <DefaultLayout>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </DefaultLayout>
+    <>
+      <DefaultLayout>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </DefaultLayout>
+      <Analytics />
+    </>
   );
 };
 
