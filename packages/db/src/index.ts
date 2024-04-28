762666dc01e5cb095e09c15a01529b432dc92991
ErickCReis <erickcorreareis@gmail.com>
2024-04-27 23:15:16 -0300

    feat: Update deps

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,13 +5,10 @@ import { connectionStr } from "./config";
 import * as auth from "./schema/auth";
 import * as team from "./schema/team";
 
-export const schema = { ...auth, ...team };
-
-export { pgSqlTable as tableCreator } from "./schema/_table";
-
 export { alias } from "drizzle-orm/pg-core";
 export * from "drizzle-orm/sql";
 
-const neonClient = neon(connectionStr.href);
+export const schema = { ...auth, ...team };
 
+const neonClient = neon(connectionStr.href);
 export const db = drizzle(neonClient, { schema });
